439c6c7914013e1bdacb72a248077ac1a4e6fbc6
uelola <80069286+uelola@users.noreply.github.com>
2021-11-09 13:53:56 +0100

    updated the initial coordinates of the snake at the start

--- SnakeOpenGL/game.cpp
+++ SnakeOpenGL/game.cpp
@@ -24,7 +24,7 @@ extern int score;
 //int posX = 32, posY = 24;
 
 // at the beginning initialize starting position of the full snake body
-int posX [60] = {32, 32, 32, 32, 32, 32, 32, 32, 32}, posY [60] = {24, 23, 22, 21, 20, 19, 18, 17, 16};
+int posX [60] = {32, 31, 30, 29, 28, 27, 26, 25, 24}, posY [60] = {24, 24, 24, 24, 24, 24, 24, 24, 24};
 
 // draw grid on the screen, function is called to initialize the dimensions of the grid
 void initGrid(int x, int y) {
