cfa63be804d7627574cb4f9a1dfe0a8e2567e242
uelola <80069286+uelola@users.noreply.github.com>
2021-11-16 16:11:49 +0100

    Added welcome & game over displays

--- SnakeOpenGL/main.cpp
+++ SnakeOpenGL/main.cpp
@@ -7,6 +7,10 @@
 #include <iostream>
 #include <fstream>
 #include <string.h>
+#include <string>
+
+#include <cstdlib>
+#include <charconv>
 
 using namespace std;
 
@@ -15,15 +19,24 @@ using namespace std;
 //frames per second
 #define FPS 5
 
+
+
 extern short sDirection;
 bool gameOver = false;
+
 
 int score = 0;
 
 void timer_callback(int);
 void display_callback();
+void welcome_display();
+void game_over_display();
 void reshape_callback(int, int);
 void keyboard_callback(int, int, int);
+void keys (unsigned char, int, int );
+
+GLvoid* font_style = GLUT_BITMAP_TIMES_ROMAN_24;
+int score_color = 1;
 
 void init(){
     // change default black background to pleasant green background
@@ -38,21 +51,34 @@ int main(int argc, char**argv) {
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
     glutInitWindowSize(800,600);
     glutCreateWindow("Snake");
+    init();
     // to initialize the display, needs display_callback
-    glutDisplayFunc(display_callback);
+    glutDisplayFunc(welcome_display); //glutDisplayFunc(display_callback);
     // called when the window is created, minimized, maximised or extended to any direction(size changed)
     glutReshapeFunc(reshape_callback);
     // should be called before main loop
     glutTimerFunc(0, timer_callback, 0);
     // accepting user input
     glutSpecialFunc(keyboard_callback);
-    init();
+    glutKeyboardFunc(keys);
+
     glutMainLoop();
     return 0;
 }
 
+void keys (unsigned char key, int x, int y){
+    if (key == 's') {
+        glutDisplayFunc(display_callback);
+    }
+    // control back to display
+    glutPostRedisplay();
+}
+
 
 void display_callback() {
+
+//     const int buffer = 100;
+//     char msg[buffer + 1];
     // clears the buffer specified by the argument
     glClear(GL_COLOR_BUFFER_BIT);
     glLoadIdentity();
@@ -66,10 +92,54 @@ void display_callback() {
        gives higher FPS if delay in not introduced */
     glutSwapBuffers();
     if(gameOver) {
-        exit(0);
+        glutDisplayFunc(game_over_display);
+        glutPostRedisplay();
+        //exit(0);
     }
+
 }
 
+void welcome_display() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glColor3f(0.498039, 1.0, 0.0);
+
+    glRasterPos3f(26,30,0);
+    char msg [] = "SNAKE GAME";
+    for(int i=0; i< strlen(msg); i++) {
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, msg[i] );
+    }
+    glRasterPos3f(24,26,0);
+    char msg1 [] = "Press S to start the game";
+    for(int i=0; i< strlen(msg1); i++) {
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, msg1[i] );
+    }
+    glutSwapBuffers();
+}
+
+void game_over_display() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glColor3f(0.498039, 1.0, 0.0);
+
+    glRasterPos3f(26,30,0);
+    char msg2 [] = "GAME OVER";
+    for(int i=0; i< strlen(msg2); i++) {
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, msg2[i] );
+    }
+    glRasterPos3f(24,26,0);
+    char msg3 [] = "Your score: " ;
+    for(int i=0; i< strlen(msg3); i++) {
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, msg3[i]);
+    }
+    glRasterPos3f(34,26,0);
+
+
+//    char msg4 [] = to_chars(score);
+//    for(int i=0; i< strlen(msg4); i++) {
+//        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, msg4[i]);
+//    }
+
+    glutSwapBuffers();
+}
 
 
 // takes 2 arguments width and height after it is resized
